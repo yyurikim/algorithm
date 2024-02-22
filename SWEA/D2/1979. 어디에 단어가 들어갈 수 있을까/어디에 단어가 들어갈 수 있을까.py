2d239892ff4e16777a28cc9dd59d86e111fd55c9
yyurikim <156268560+yyurikim@users.noreply.github.com>
2024-02-22 14:20:32 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 119 ms, Memory: 52,236 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,31 +1,28 @@
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
+
+for tc in range(1, T+1):
     N, K = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
+    puzzle += list(map(list,zip(*puzzle)))
 
-    count_list = []
-    for i in range(N):
-        count_blank = 0
+    result = 0
+    cnt = 0
+
+    for i in range(2*N):
         for j in range(N):
-            if lst[i][j] == 0:
-                count_list.append(count_blank)
-                count_blank = 0
-            elif lst[i][j] == 1:
-                count_blank += 1
+            if puzzle[i][j] == 1:
+                cnt += 1
                 if j == N-1:
-                    count_list.append(count_blank)
+                    if cnt == K:
+                        result += 1
+                        cnt = 0
+                    else:
+                        cnt = 0
+            elif puzzle[i][j] == 0:
+                if cnt == K:
+                    result += 1
+                    cnt = 0
+                else:
+                    cnt = 0
 
-    count_blank = 0
-    for j in range(N):
-        count_blank = 0
-        for i in range(N):
-            if lst[i][j] == 0:
-                count_list.append(count_blank)
-                count_blank = 0
-            elif lst[i][j] == 1:
-                count_blank += 1
-                if i == N-1:
-                    count_list.append(count_blank)
-
-    print(f'#{test_case} {count_list.count(K)}')
+    print(f'#{tc} {result}')
