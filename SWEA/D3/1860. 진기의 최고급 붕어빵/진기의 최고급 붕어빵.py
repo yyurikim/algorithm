d3c735f9477a385de0e489df6072d31f46ebaadb
yyurikim <156268560+yyurikim@users.noreply.github.com>
2024-02-29 09:18:43 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 444 ms, Memory: 62,124 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,30 +1,18 @@
 T = int(input())
-
 for tc in range(1, T+1):
     N, M, K = map(int, input().split())
-    cust_info = list(map(int, input().split()))
+    customer = list(map(int, input().split()))
+
+    customer.sort()
+    cnt = 0
     result = 'Possible'
-    b_lst = [0] * 11112
-    b_idx = 0
 
-    for i in range(0, 11112, M):
-        b_lst[i] = K
-
-    b_lst[0] = 0
-
-    for j in cust_info:
-        if b_lst[j] != 0:
-            b_lst[j] -= 1
+    for i in range(len(customer)):
+        bung = (customer[i] // M) * K - cnt
+        if bung > 0:
+            cnt += 1
         else:
-            if sum(b_lst[:j+1]) <= 0:
-                result = 'Impossible'
-                break
-            else:
-                for k in range(j, 0, -1):
-                    if b_lst[k] > 0:
-                        b_idx = k
-                        break
-                b_lst[b_idx] -= 1
-
+            result = 'Impossible'
+            break
 
     print(f'#{tc} {result}')
