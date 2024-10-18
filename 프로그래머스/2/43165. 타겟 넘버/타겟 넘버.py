8e1e556ec66cd74ee5baf0a72251c885bfc04cfd
Yuri Kim <156268560+yyurikim@users.noreply.github.com>
2024-10-18 18:39:15 +0900

    [level 2] Title: 타겟 넘버, Time: 518.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,19 @@
-cnt = 0
+answer = 0
 
 def solution(numbers, target):
+
+    
     def dfs(idx, sum_val):
-        global cnt
+        global answer
         if idx == len(numbers):
             if sum_val == target:
-                cnt += 1
+                answer += 1
             return
-
+        
         dfs(idx+1, sum_val + numbers[idx])
-        dfs(idx+1, sum_val - numbers[idx])    
-
-    dfs(0, 0)
-
-    return cnt
+        dfs(idx+1, sum_val - numbers[idx])
+        
+    
+    dfs(0,0)
+    
+    return answer
